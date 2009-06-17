77e16fa6808fc1eee3ec6a925eeb6b726be49198
ziscloud <ziscloud@41768476-33d5-11de-96ab-f16d9912eac9>
2009-06-17 02:39:07 +0000

    add the abstract class for the optimization

---
+++ com.ziscloud.zcdiagram/src/com/ziscloud/zcdiagram/optimize/AOptimize.java
@@ -0,0 +1,41 @@
+package com.ziscloud.zcdiagram.optimize;
+
+import java.util.List;
+
+import com.ziscloud.zcdiagram.pojo.Activity;
+
+public abstract class AOptimize {
+	/**
+	 * the activities as the optimization's input
+	 */
+	protected List<Activity> activities;
+
+	/**
+	 * construct the optimization using the giving activities
+	 * 
+	 * @param activities
+	 *            the activities as the optimization's input
+	 */
+	public AOptimize(List<Activity> activities) {
+		this.activities = activities;
+	}
+
+	/**
+	 * the optimization using the model one.
+	 * 
+	 * @return the start date and end date of the activities. String[0] is the
+	 *         start date, String[1] is the end date, and they are also an array
+	 *         with the same length.
+	 */
+	abstract public String[][] modelOneOptimize();
+
+	/**
+	 * the optimization using the model two
+	 * 
+	 * @return the start date and end date of the activities. String[0] is the
+	 *         start date, String[1] is the end date, and they are also an array
+	 *         with the same length.
+	 */
+	abstract public String[][] modelTwoOptimize();
+
+}
