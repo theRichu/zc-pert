e488dd8aa5165b46453399e968bc662ee7417d01
ziscloud <ziscloud@41768476-33d5-11de-96ab-f16d9912eac9>
2010-03-19 04:56:47 +0000

    modify the route to be default

--- trunk/contact_list/config/routes.rb
+++ trunk/contact_list/config/routes.rb
@@ -23,12 +23,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :projects, :collection => { :groups => :get }
 
-  map.connect 'contact_list', :controller=>'contact_list', :action=>'index'
-
-  map.connect 'administrator', :controller=>"administrator", :action=>"index"
-
-  map.connect 'projects/groups', :controller=>'projects', :action=>'groups'
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -68,6 +62,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  # map.connect ':controller/:action/:id'
-  # map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 end
